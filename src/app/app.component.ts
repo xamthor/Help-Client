dee625a5b863e1ecd5856f3526076a615a31c29f
xam <admin@xbitestudio.com>
2021-01-10 05:20:15 -0600

    Dark mode toggle moved to menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../environments/environment';
-import { faLightbulb as solid } from '@fortawesome/free-solid-svg-icons';
-import { faLightbulb as regular } from '@fortawesome/free-regular-svg-icons';
-
 
 @Component({
   selector: 'app-root',
@@ -12,16 +9,10 @@ import { faLightbulb as regular } from '@fortawesome/free-regular-svg-icons';
 export class AppComponent implements OnInit{
 	title: string = 'Help';
 	register: string = "false";
-	Lightsolid = solid;
-	Lightregular = regular;
 
 	ngOnInit(): void {
 		this.darkmode()
 		console.log(environment.production); // Logs false for default environment
-	}
-
-	darktoggle(){
-		if(localStorage.theme == 'dark'){localStorage.theme = 'light'}else{localStorage.theme = 'dark'}
 	}
 
 	darkmode(): boolean{
--- src/app/components/menu-screen/menu-screen.component.ts
+++ src/app/components/menu-screen/menu-screen.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {UserAccountService} from '../../services/user-account.service';
 import {Router} from '@angular/router';
+import { faLightbulb as solid } from '@fortawesome/free-solid-svg-icons';
+import { faLightbulb as regular } from '@fortawesome/free-regular-svg-icons';
+
 
 @Component({
   selector: 'app-menu-screen',
@@ -8,6 +11,9 @@ import {Router} from '@angular/router';
   styleUrls: ['./menu-screen.component.css']
 })
 export class MenuScreenComponent implements OnInit {
+
+  Lightsolid = solid;
+  Lightregular = regular;
 
   constructor(private userAccountService: UserAccountService, private router:Router) { }
 
@@ -22,4 +28,15 @@ export class MenuScreenComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  darktoggle(){
+    if(localStorage.theme == 'dark'){localStorage.theme = 'light'}else{localStorage.theme = 'dark'}
+  }
+
+darkmode(): boolean{
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
