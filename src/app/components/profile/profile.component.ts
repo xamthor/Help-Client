71257695934fff527f981fff61913abb8c8b6a57
Smiley <jcsmileyjr@gmail.com>
2021-01-15 21:07:54 -0600

    Feature: User can update their personal details on the profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -16,13 +16,7 @@ export class ProfileComponent implements OnInit {
   titleFirstName: string = "First Name"; // Input field title and sets the icon in the input field
   titleLastName: string = "Last Name";   // Input field title and sets the icon in the input field
 
-  newUserName: string = "";  // variable updated from the input designiated input field and then used to update the temp User
-  newUserEmail: string = ""; // variable updated from the input designiated input field and then used to update the temp User
-  newUserFname: string = ""; // variable updated from the input designiated input field and then used to update the temp User
-  newUserLname: string = ""; // variable updated from the input designiated input field and then used to update the temp User
-  newUserPhone: string = ""; // variable updated from the input designiated input field and then used to update the temp User
-
-
+  
   updatedCurrentUser :User = {
     userName: "",
     password: "",
@@ -36,50 +30,42 @@ export class ProfileComponent implements OnInit {
 
   constructor(private router:Router, private userAccountServices: UserAccountService, private profileService: ProfileService) { 
     this.updatedCurrentUser = this.userAccountServices.getCurrentUser(); // Update the user profile with the global currenet user'profile
-    console.log(this.updatedCurrentUser);
   }
 
   ngOnInit(): void {
   }
 
-  updateProfile(){
-    
+  // Method called to update the global current user data, back-end database, and route the user back to the feed content page
+  updateProfile(){    
     this.userAccountServices.create(this.updatedCurrentUser);// Update the global current user's profile
     this.profileService.updateProfile(this.updatedCurrentUser).subscribe(data => {
-      //this.arrayOfProfiles = data.data;
-      console.log(data); //TESTING
       this.router.navigate(['/feed']);
+      // TODO: Inform the user that the update was successful
     })
-    console.log(this.updatedCurrentUser);
   }
 
   // Capture user input from input field
   getUserName($event:any){
-    //this.newUserName = $event;
     this.updatedCurrentUser.userName = $event;
   }
 
   // Capture user input from input field
   getEmail($event:any){
-    //this.newUserEmail = $event;
     this.updatedCurrentUser.email = $event;
   }
 
-      // Capture user input from input field
+  // Capture user input from input field
   getFirstName($event:any){
-    //this.newUserFname = $event;
     this.updatedCurrentUser.firstName = $event;
   }  
 
   // Capture user input from input field
   getLastName($event:any){
-    //this.newUserLname = $event;
     this.updatedCurrentUser.lastName = $event;
   }
 
   // Capture user input from input field
   getPhone($event:any){
-    //this.newUserPhone = $event;
     this.updatedCurrentUser.phoneNumber = $event;
   }
 
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,19 +2,26 @@ import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import {AuthenticateService} from '../services/authenticate.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// Service used on the profile page/component to update the current user's personal details 
 export class ProfileService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authenticateUser: AuthenticateService, private cookieService: CookieService) { }
 
-  // Used on the log-in page to update the app by validating the username/password from the back-end database
+  // Used on the profile page to update a detail about the currenet user
   updateProfile(updatedUser: User){
+    const authToken = this.authenticateUser.getAuthToken();
+    this.cookieService.set('jwt',authToken);
     const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
+    let options = { headers: headers, withCredentials: true };
     var raw = JSON.stringify({"email":updatedUser.email,"phoneNumber":updatedUser.phoneNumber, "firstName":updatedUser.firstName,"lastName":updatedUser.lastName, "userName":updatedUser.userName});
-    return this.http.post<any>(`${environment.apiEndPointRoute}/profile/update`, raw,{headers: headers})
+    return this.http.post<any>(`${environment.apiEndPointRoute}/profile/update`, raw, options)
   }
 
 
